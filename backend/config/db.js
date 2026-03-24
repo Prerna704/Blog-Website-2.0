a86ed60f13d6512c31f15e4b6da2e710edbd255c
Prerna704 <prernaprem720@gmail.com>
2026-03-24 11:19:09 +0530

    update mongodb url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+  await mongoose.connect("mongodb+srv://prernaprem720_db_user:6xkV5ydgemn0bsUd@cluster0.ohqsubs.mongodb.net/blogDB");
   console.log("MongoDB Connected");
-};
+};//6xkV5ydgemn0bsUd
 
 module.exports = connectDB;
